4b0e302ac199efd917b22311fab616b7f130cbd4
Alena Fomenkova <fruitella30sep@mail.ru>
2020-01-06 22:14:38 +0300

    Updating configuration.

--- gulpfile.js
+++ gulpfile.js
@@ -1,93 +1,36 @@
-"use strict";
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const plumber = require('gulp-plumber');
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
+const del = require('del');
+const csso = require('gulp-csso');
+const uglify = require('gulp-uglify-es').default;
+const concat = require('gulp-concat');
 
-var gulp = require("gulp"); // сам сборщик
-var sass = require("gulp-sass"); // преобразует контент из sass в css
-var plumber = require("gulp-plumber"); // показывает ошибки в консоли и продолжает выполнение потока (не останавливает выполнение скрипта)
-var postcss = require("gulp-postcss"); // плагин для преобразования сss
-var posthtml = require("gulp-posthtml"); // парсер HTML
-var autoprefixer = require("autoprefixer"); // автопрефексер, работает в потоке postcss
-var server = require("browser-sync").create(); // автоматически перезагружает страницу
-var del = require("del"); // плагин для удаления файлов и папок
-var rename = require("gulp-rename"); // плагин для переименования файлов
-var csso = require("gulp-csso"); // минификатор css
-var uglify = require("gulp-uglify"); // минификатор JS
-var concat = require("gulp-concat"); // плагин для объединения файлов
+gulp.task('clean', () => del('build'));
 
-gulp.task("copy", function () {
-  return gulp.src([
-    "source/js/**",
-    "source/*.html"
-  ], {
-      base: "source"
-    })
-    .pipe(gulp.dest("build"));
-});
+gulp.task('css', () => gulp.src('source/sass/style.scss')
+  .pipe(plumber())
+  .pipe(sass({
+    includePaths: 'node_modules/normalize-scss/sass',
+  }))
+  .pipe(postcss([
+    autoprefixer(),
+  ]))
+  .pipe(csso())
+  .pipe(gulp.dest('build')));
 
-gulp.task("clean", function () {
-  return del("build");
-});
+gulp.task('js', () => gulp.src([
+  'source/js/accordion.js',
+  'source/js/onoffswitch.js',
+])
+  .pipe(concat('script.js'))
+  .pipe(uglify())
+  .pipe(gulp.dest('build')));
 
-gulp.task("css", function () {
-  return gulp.src("source/sass/style.scss")
-    .pipe(plumber())
-    .pipe(sass())
-    .pipe(postcss([
-      autoprefixer()
-    ]))
-    .pipe(gulp.dest("build/css"))
-    .pipe(csso())
-    .pipe(rename("style.min.css"))
-    .pipe(gulp.dest("build/css"))
-    .pipe(server.stream());
-});
-
-// gulp.task("html", function () {
-//   return gulp.src("source/*.html")
-//     .pipe(posthtml([]))
-//     .pipe(rename(function (path) {
-//       path.basename += ".min";
-//     }))
-//     .pipe(gulp.dest("build"));
-// });
-
-gulp.task("indexjs", function () {
-  return gulp.src([
-    // "source/js/map.js",
-    // "source/js/menu-toggle.js",
-    // "source/js/slider.js",
-    // "source/js/picturefill.min.js",
-    // "source/js/svg4everybody.min.js"
-  ])
-    .pipe(concat("index.js"))
-    .pipe(uglify())
-    .pipe(rename("index.min.js"))
-    .pipe(gulp.dest("build/js"))
-});
-
-gulp.task("build", gulp.series(
-  "clean",
-  "copy",
-  "css",
-  //"html",
-  //"indexjs",
+gulp.task('build', gulp.series(
+  'clean',
+  'css',
+  'js',
 ));
-
-gulp.task("server", function () {
-  server.init({
-    server: "build/",
-    notify: false,
-    open: true,
-    cors: true,
-    ui: false
-  });
-  gulp.watch("source/sass/**/*.{scss,sass}", gulp.series("css"));
-  //gulp.watch("source/js/*.js", gulp.series("indexjs", "refresh"));
-  //gulp.watch("source/*.html", gulp.series("html", "refresh"));
-});
-
-gulp.task("refresh", function (done) {
-  server.reload();
-  done();
-});
-
-gulp.task("start", gulp.series("build", "server"));
